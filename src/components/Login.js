a0769ef7e76a46d761d6d6b5d08cf6970bcaeeeb
Codabae <shalom.mat40@gmail.com>
2018-10-23 13:13:56 +0100

    dashboard

--- src/components/Login.js
+++ src/components/Login.js
@@ -34,8 +34,9 @@ export default class Login extends Component {
                 <br />
 
                 <div className=''>
-                  
+                  <Link to='Dashboard'>
                     <Button type='dashed' block>Ride On</Button>
+                    </Link >
                 </div>
 
                 <div className='IconDF'>
